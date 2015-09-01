b6fcb5496f540b2595db7d948b2a57beb6ca1994
Maurício Aniche <mauricioaniche@gmail.com>
2015-09-01 18:32:32 -0300

    italic

--- documentation.php
+++ documentation.php
@@ -113,7 +113,7 @@
 
         <p>
         You can get the list of modified files, as well as their diffs and current
-        source code. To that, all you have to do is to get the list of <i>Modification<i>s
+        source code. To that, all you have to do is to get the list of <i>Modification</i>s
         that exists inside <i>Commit</i>. A <i>Commit</i> contains a hash, a committer (name
         and email), a message, the date, its parent hash, and the list of modification.
 
