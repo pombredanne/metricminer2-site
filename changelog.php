5d452f4e41b7725a6ed96a3867dc2cbeef5cb39e
Maurício Aniche <mauricioaniche@gmail.com>
2015-09-05 19:31:34 -0300

    tagging the changelog

--- changelog.php
+++ changelog.php
@@ -6,14 +6,14 @@
     <div class="col-lg-12">
         <h1>Team</h1>
 
-        <h2>2.0.1</h2>
+        <h2 id="201">2.0.1</h2>
 
         <ul>
         <li>No more implementation of code metrics anymore. JDT and ANTLR parsers can still be used, but no implementation of CC, NOM, etc, is provided by the framework</li>
         <li>New commits range: ListOfCommits, Range</li>
         </ul>
 
-        <h2>2.0.0</h2>
+        <h2 id="200">2.0.0</h2>
 
         <ul>
             <li>First release ever! \o/</li>
